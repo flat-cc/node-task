e262ede4422b55102a9acc07f086c6c033696b2b
hyc <yongchuan.huang@socialcredits.cn>
2023-01-13 13:58:06 +0800

    🗑️ delete: 删除多余日志

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,12 @@ log('🚀 启动任务~')
 
 // 贝壳定时任务启动~每日9点
 schedule.scheduleJob('0 0 9 * * 0-7', () => {
-  log('贝壳数据开始抓取~', dayjs().format('YYYY-MM-DD HH:mm:ss'))
+  log('贝壳数据开始抓取~')
   beikeTask()
 });
 
 // 开源中国~每日9点40
 schedule.scheduleJob('0 40 9 * * 1-5', function(){
-  log('开源中国每日资讯抓取~', dayjs().format('YYYY-MM-DD HH:mm:ss'))
+  log('开源中国每日资讯抓取~')
   osChinaTask()
 });
