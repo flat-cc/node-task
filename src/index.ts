d4781c10cb83d2d13d5c7beb7bc1add3c550cbbb
hyc <yongchuan.huang@socialcredits.cn>
2023-02-27 17:15:17 +0800

    🚀 build: 修改运行命令

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,22 @@ import getCommunity from "./features/beike/community";
 
 log('🚀 启动任务~')
 
-// // 贝壳定时任务启动~每日9点
-// schedule.scheduleJob('0 0 9 * * 0-7', () => {
-//   log('贝壳数据开始抓取~')
-//   beikeTask()
-// });
+// 贝壳定时任务启动~每日9点
+schedule.scheduleJob('0 0 9 * * 0-7', () => {
+  log('贝壳数据开始抓取~')
+  beikeTask()
+  
+});
 
-// // 开源中国~每日9点40
-// schedule.scheduleJob('0 40 9 * * 1-5', function(){
-//   log('开源中国每日资讯抓取~')
-//   osChinaTask()
-// });
+// 贝壳小区定时任务启动~每日8点40
+schedule.scheduleJob('0 40 8 * * 0-7', () => {
+  log('贝壳数据开始抓取~')
+  getCommunity()
+});
 
-getCommunity()
+// 开源中国~每日9点40
+schedule.scheduleJob('0 40 9 * * 1-5', function(){
+  log('开源中国每日资讯抓取~')
+  osChinaTask()
+});
+
